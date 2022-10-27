a0b751bbea84a62211957aa4260c42bcb5f48423
Hitesh khatri <hiteshkhatridav18@gmail.com>
2022-10-27 13:19:23 +0530

    hdy

---
+++ quiz.js
@@ -0,0 +1,100 @@
+function c1() {
+    var text = document.getElementById("a1");
+    var a = text.value;
+    if (a == "divyanshi") {
+        document.getElementById("demo1").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo1").innerHTML =
+            "Your answer is wrong. correct answer is divyanshi.";
+    }
+}
+function c2() {
+    var text = document.getElementById("a2");
+    var a = text.value;
+    if (a == "yashika") {
+        document.getElementById("demo2").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo2").innerHTML =
+            "Your answer is wrong. correct answer is yashika.";
+    }
+}
+function c3() {
+    var text = document.getElementById("a3");
+    var a = text.value;
+    if (a == "aarathi") {
+        document.getElementById("demo3").innerHTML = "Can't revel you all know why";
+    } else {
+        document.getElementById("demo3").innerHTML =
+            "Your answer is wrong.";
+    }
+}
+function c4() {
+    var text = document.getElementById("a4");
+    var a = text.value;
+    if (a == "ansh") {
+        document.getElementById("demo4").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo4").innerHTML =
+            "Your answer is wrong. correct answer is ansh.";
+    }
+}
+/*function c5() {
+    var text = document.getElementById("a5");
+    var a = text.value;
+    if (a == "arunachal pradesh") {
+        document.getElementById("demo5").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo5").innerHTML =
+            "Your answer is wrong. correct answer is arunachal pradesh.";
+    }
+}
+function c6() {
+    var text = document.getElementById("a6");
+    var a = text.value;
+    if (a == "1947") {
+        document.getElementById("demo6").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo6").innerHTML =
+            "Your answer is wrong. correct answer is 1947.";
+    }
+}
+function c7() {
+    var text = document.getElementById("a7");
+    var a = text.value;
+    if (a == "november") {
+        document.getElementById("demo7").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo7").innerHTML =
+            "Your answer is wrong. correct answer is november.";
+    }
+}
+function c8() {
+    var text = document.getElementById("a8");
+    var a = text.value;
+    if (a == "1950") {
+        document.getElementById("demo8").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo8").innerHTML =
+            "Your answer is wrong. correct answer is 1950.";
+    }
+}
+function c9() {
+    var text = document.getElementById("a9");
+    var a = text.value;
+    if (a == "mango") {
+        document.getElementById("demo9").innerHTML = "Your answer is correct";
+    } else {
+        document.getElementById("demo9").innerHTML =
+            "Your answer is wrong. correct answer is mango.";
+    }
+}
+    function c10() {
+        var text = document.getElementById("a10");
+        var a = text.value;
+        if (a == "peacock") {
+            document.getElementById("demo10").innerHTML = "Your answer is correct";
+        } else {
+            document.getElementById("demo10").innerHTML =
+                "Your answer is wrong. correct answer is peacock.";
+        }
+    }*/
